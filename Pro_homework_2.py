7262eaebc33fae891a1452459ef58e5b22fee4ca
Gydermes <110401357+Gydermes@users.noreply.github.com>
2022-08-21 17:16:37 +0300

    Add files via upload

--- Pro_homework_2.py
+++ Pro_homework_2.py
@@ -16,13 +16,11 @@ class Person:
         return f'{self.surname} {self.name}'
 
 
-
 st_1 = module_3.Student('Ivanov', 'Ivan', '12-12-12')
 st_2 = module_3.Student('Ivanov1', 'Ivan1', '12-12-12')
 st_3 = module_3.Student('Ivanov1', 'Ivan2', '12-12-12')
 st_4 = module_3.Student('Ivanov', 'Ivan3', '12-12-12')
 st_5 = module_3.Student('Ivanov', 'Ivan3', '12-12-12')
-
 
 gr = module_4.Group('234234')
 gr.add_student(st_1)
