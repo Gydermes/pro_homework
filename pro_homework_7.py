3473a839308285522d1504703ac7db2eb9857a21
Yaroslav <110401357+Gydermes@users.noreply.github.com>
2022-08-27 23:47:09 +0300

    Add files via upload

--- pro_homework_7.py
+++ pro_homework_7.py
@@ -1,3 +1,4 @@
+# Exercises 1
 """Реализуйте генераторную функцию, которая будет возвращать по
 одному члену геометрической прогрессии с указанным множителем.
 Генератор должен остановить свою работу или по достижению указанной
@@ -14,3 +15,20 @@ def gen_func(start, stop):
 
 x = gen_func(10, 20)
 print(x)
+for item in x:
+    print(item)
+
+print('****************')
+# Exercises 2
+
+
+def test():
+    yield from range(5)
+
+
+for i in test():
+    print(i)
+
+# Exercises 4
+a = [x**3 for x in range(1, 10)]
+print(a)
