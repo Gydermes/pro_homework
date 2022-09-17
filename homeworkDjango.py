bf9978a4318c2c99c04466b0fd39c94955d6abf6
Yaroslav <110401357+Gydermes@users.noreply.github.com>
2022-09-17 17:20:45 +0300

    Add files via upload

--- homeworkDjango.py
+++ homeworkDjango.py
@@ -1,22 +1,5 @@
 import re
 
-
-def login(login_name: str):
-    if not isinstance(login_name, str):
-        raise TypeError()
-    regex = "^[a-zA-Z0-9]+$"
-    res = re.compile(regex)
-    if 2 <= len(login_name) <= 10 and res.search(login_name) is not None:
-        return login_name
-    return "correct login"
-
-
-res = login('2562*')
-print(res)
-
-import re
-
-
 s = 'GfdfRb kRbrasd sadRbbr Rbr 24Rbbbrsasd'
 res = re.findall(r'Rbr|Rbbr|Rbbbr', s)
 if res:
@@ -25,25 +8,39 @@ else:
     print('None')
 
 
-# import validate v
-# def number_cart(cart:int):
-#     if not isinstance(cart, int):
-#         raise TypeError()
-#     print(v.lengh(16).validate(cart))
-#
-#
-# res = number_cart(1616161616161616)
+def cart(number):
+    if re.findall(r'^[\d]{16}$', number):
+
+        return 'card number entered correctly'
+
+    return "wrong cart"
 
 
-import re
+res = cart('1234567890123456')
+print(res)
 
 
 def mail(name):
-    if re.findall(r'^[a-zA-Z0-9]+$', name):
+    if re.findall(r'^[a-zA-Z\d_-]+@[a-zA-Z\d]+.[a-z]+$', name):
+
         return name
 
-    return "correct mail"
+    return "wrong mail"
 
 
-res = mail('Gydermes23')
+res = mail('fmk26hbgh@gmail943.com')
 print(res)
+
+
+def login(login_name: str):
+    if not isinstance(login_name, str):
+        raise TypeError()
+    res = re.compile(r'^[a-zA-Z\d]+$')
+    if 2 <= len(login_name) <= 10 and res.search(login_name) is not None:
+        return login_name
+    return "wrong login"
+
+
+result = login('23eSSasz')
+print(result)
+
